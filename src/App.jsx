35a3e02a9b6db57a8fb402ff3245b171c01a8b13
unknown <randyleean092m@gmail.com>
2024-01-31 19:43:01 +1300

    feat: added css and html configuration for the page. How to center a div or main?

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import Meme from "./components/Meme";
 export default function App() {
   return (
     <>
-      <Header></Header>
-      <Meme></Meme>
+      <div>
+        <Header></Header>
+        <Meme></Meme>
+      </div>
     </>
   );
 }
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -3,9 +3,18 @@ import React from "react";
 export default function Meme() {
   return (
     <main>
-      <form>
-        <input></input>
-        <input></input>
+      <form className="form">
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Top Text"
+        ></input>
+        <input
+          type="text"
+          className="form--input"
+          placeholder="Bottom Text"
+        ></input>
+        <button className="form--button">Get a new meme image</button>
       </form>
     </main>
   );
