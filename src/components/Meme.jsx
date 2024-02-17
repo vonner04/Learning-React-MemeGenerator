a0f9e8e00d3363106b9be3edeb862197a3bea006
unknown <randyleean092m@gmail.com>
2024-02-18 01:36:18 +1300

    bugFix: meme image now renders when pressing generate image button.

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -3,13 +3,32 @@ import { useState } from "react";
 import memesData from "../memesData.js";
 
 export default function Meme() {
-  const [memeImage, setMemeImage] = useState("");
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    memeImage: "",
+  });
 
   function getMemeImage() {
     const memesArray = memesData.data.memes;
-    const randomMeme =
-      memesArray[Math.floor(Math.random() * memesArray.length)];
-    setMemeImage(randomMeme.url);
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        memeImage: url,
+      };
+    });
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        [name]: value,
+      };
+    });
   }
 
   return (
@@ -19,17 +38,27 @@ export default function Meme() {
           type="text"
           className="form--input"
           placeholder="Top Text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
         ></input>
         <input
           type="text"
           className="form--input"
           placeholder="Bottom Text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
         ></input>
         <button className="form--button" onClick={getMemeImage}>
           Get a new meme image
         </button>
       </div>
-      <img src={memeImage} className="meme--image" />
+      <div className="meme">
+        <img src={meme.memeImage} className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 }
